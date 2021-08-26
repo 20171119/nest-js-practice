c2184bf56e740ff9072a8dc5119a8c306e0d384c
20171119 <jspark98@unist.ac.kr>
2021-08-27 01:30:02 +0900

    postgresql CRUD

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { Board, BoardStatus } from './board.model';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BoardStatus } from './board-status.enum';
+import { Board } from './board.entity';
 import { BoardsService } from './boards.service';
 import { CreateBoardDto } from './dto/create-board-dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
@@ -9,28 +10,28 @@ export class BoardsController {
     constructor(private boardService: BoardsService) {}
 
     @Get()
-    getAllBoard(): Board[] {
+    getAllBoard(): Promise<Board[]> {
         return this.boardService.getAllBoards();
     }
 
     @Post()
     @UsePipes(ValidationPipe)
-    createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
         return this.boardService.createBoard(createBoardDto);
     }
 
     @Get('/:id')
-    getBoardById(@Param('id') id: string): Board {
-        return this.boardService.getBoardById(id)
+    getBoardById(@Param('id') id: number): Promise<Board> {
+        return this.boardService.getBoardById(id);
     }
 
     @Delete('/:id')
-    deleteBoard(@Param('id') id: string): void {
-        this.boardService.deleteBoard(id);
+    deleteBoard(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        return this.boardService.deleteBoard(id);
     }
 
     @Patch('/:id/status')
-    updateBoardStatus(@Param('id') id: string, @Body('status', BoardStatusValidationPipe) status: BoardStatus): Board {
+    updateBoardStatus(@Param('id', ParseIntPipe) id: number, @Body('status', BoardStatusValidationPipe) status: BoardStatus): Promise<Board> {
         return this.boardService.updateBoardStatus(id, status);
     }
 }
